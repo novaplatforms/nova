7b930b7119c20200fbc6f0d205d9ba7a86b867cd
gnatykdm <gnatykwork@gmail.com>
2024-08-12 11:34:42 +0200

    changed-framework-to-react

--- web-client/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'login', redirectTo: 'app-login', pathMatch: 'full'},
-  {path: 'register', redirectTo: 'app-registration', pathMatch: 'full'},
-  {path: 'info', redirectTo: 'app-info', pathMatch: 'full'},
-  {path: 'profile', redirectTo: 'app-profile', pathMatch: 'full'},
-];
